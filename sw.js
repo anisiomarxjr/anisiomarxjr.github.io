82ca3c97a45e15d2d35502a9ebe68f34e9aa56fe
Anisio Marques Junior <anisiomarxjr@Anisios-MacBook-Pro.local>
2017-12-06 09:50:57 -0200

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-4245be2d721ba1d10658.js","d5bbbf82289741b89f4b68dc7fde1cc8"],["/app-4457c999c9818c93719b.js","96b851a0394f17967a084e39e2edddd8"],["/app-9b3e51842b2ba18c131f.js","f9a0b7eca948f03be9e7d285a765dbf5"],["/commons-b9295c1fa34e18bb3199.js","94e0a9c73d5b17dedc09dc507d5a016c"],["/index.html","8a608eac5acc914dfe3c34161f60fccd"],["/offline-plugin-app-shell-fallback/index.html","f398b6f6bb2b027ed67c227820fb0a41"]];
+var precacheConfig = [["/app-4245be2d721ba1d10658.js","d5bbbf82289741b89f4b68dc7fde1cc8"],["/app-4457c999c9818c93719b.js","96b851a0394f17967a084e39e2edddd8"],["/app-9b3e51842b2ba18c131f.js","f9a0b7eca948f03be9e7d285a765dbf5"],["/commons-b9295c1fa34e18bb3199.js","94e0a9c73d5b17dedc09dc507d5a016c"],["/index.html","9bbc390c521b1a708476aa76574738ca"],["/offline-plugin-app-shell-fallback/index.html","62051775c33960e2ac0239e532953592"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
